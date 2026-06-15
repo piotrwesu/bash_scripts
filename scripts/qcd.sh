1f8e90e3eff1fe4202c970ff8e4b6aa92361e186
Piotr Wesu <piotrwesu@gmail.com>
2026-06-15 18:53:39 +0200

    added qcd.sh

---
+++ scripts/qcd.sh
@@ -0,0 +1,18 @@
+qcd()
+{
+    case "$1" in
+        VoidLook)
+            cd ~/Repos/CNetwork/VoidLook
+            ;;
+        GeminiCli)
+            cd ~/Repos/GeminiCli
+            ;;
+        *)
+            echo "qcd: unkhnow key'$1'"
+            return 1;
+            ;;
+    esac
+    pwd
+}
+
+complete -W "VoidLook GeminiCli" qcd
